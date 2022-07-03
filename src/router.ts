058b7181446b3965ed6b6ff5715315a5d609ba28
efoejean <efoejean@gmail.com>
2022-07-02 21:13:16 -0500

    set up routes

---
+++ src/router.ts
@@ -0,0 +1,42 @@
+import express, { Request, Response } from 'express';
+import itemsContollers from './controller/items.contollers';
+
+const router = express.Router();
+
+router.get('/', (req: Request, res: Response) => {
+  res.json('Hello World!');
+});
+
+router.get('/items', async (req: Request, res: Response) => {
+  res.json(await itemsContollers.find(req.query));
+});
+
+router.get('/items/:id', async (req: Request, res: Response) => {
+  try {
+    res.json(await itemsContollers.findOne(req.params.id!));
+  } catch {
+    res.status(404).json({ message: 'Item not found' });
+  }
+});
+
+router.post('/items/insert', async (req: Request, res: Response) => {
+  res.json(await itemsContollers.insert(req.body));
+});
+
+router.put('/items/:id', async (req: Request, res: Response) => {
+  try {
+    res.json(await itemsContollers.update(req.params.id!, req.body));
+  } catch {
+    res.status(404).json({ message: 'Item not found' });
+  }
+});
+
+router.delete('/items/delete/:id', async (req: Request, res: Response) => {
+  console.log();
+  try {
+    res.json(await itemsContollers.delete(req.params.id!));
+  } catch {
+    res.status(404).json({ message: 'Item not found' });
+  }
+});
+export default router;
